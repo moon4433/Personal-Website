e1098ce72b8b5519b81b9a576280608db752427d
moon4433 <johnk117@ferris.edu>
2023-01-03 00:50:46 -0500

    Made some major changes

---
+++ public/js/header.js
@@ -0,0 +1,13 @@
+
+
+function setup() {
+  let cnv = createCanvas(displayWidth, displayHeight);
+  cnv.parent('header-canvas');
+  // Create objects
+  
+}
+
+function draw() {
+  background(18, 18, 18);
+  
+}
