89a5897e7c1e59c380d35afe4a5e7a6f23f97762
Keegan <johnk117@ferris.edu>
2023-01-03 20:22:46 -0500

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
-});
+}); 
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Server is listening on port 3000...');
