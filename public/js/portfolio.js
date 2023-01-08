d20f59046e93fc087b20b6ebf92d02a714efe7aa
Keegan <johnk117@ferris.edu>
2023-01-07 23:43:16 -0500

    Finishing up layout

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,5 +1,6 @@
 let highlight = document.getElementById('highlighter');
 let menuOptions = document.querySelectorAll('#portfolio-menu .portfolio__menu-item');
+let workOptions = document.querySelectorAll('#portfolio-work-menu .work');
 
 window.addEventListener('resize', ()=>{
     menuOptions.forEach((item) => {
@@ -32,6 +33,25 @@ menuOptions.forEach((option) => {
                 highlight.style.height = item.offsetHeight + 'px';
             }
         });
+    });
+
+    option.addEventListener('click',(event)=>{
+        
+    });
+});
+
+workOptions.forEach((work)=>{
+    work.addEventListener('mouseenter', (event)=>{
+        work.querySelector('.work__overlay').classList.add('hovered');
+        work.querySelector('.work__title').classList.add('visible');
+        work.querySelector('.work__desc').classList.add('visible');
+        work.querySelector('.work__button').classList.add('visible');
+    });
+    work.addEventListener('mouseleave', (event)=>{
+        work.querySelector('.work__overlay').classList.remove('hovered');
+        work.querySelector('.work__title').classList.remove('visible');
+        work.querySelector('.work__desc').classList.remove('visible');
+        work.querySelector('.work__button').classList.remove('visible');
     });
 });
 
