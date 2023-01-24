db12abc351217868bdcbb56de5e2667bb685e65f
moon4433 <johnk117@ferris.edu>
2023-01-24 00:19:57 -0500

    Added Files

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const https = require('https');
 const bodyParser = require('body-parser');
 const ejs = require("ejs");
-const _ = require('lodash');
 
 const app = express();
 
@@ -74,6 +73,7 @@ app.get('/game-two', function(req, res) {
 app.get('/game-three', function(req, res) {
   res.render('game-three', {  });
 });
+
 
 app.listen(process.env.PORT || 3000, function() {
   console.log('Server is listening on port 3000...');
